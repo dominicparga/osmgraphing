f56b4b7652c6edd62fc030ba0aeb6c676581661e
dominicparga <dominic.parga@gmail.com>
2019-06-05 11:50:21 +0200

    adds running proprietary xml-reader

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
 mod graph;
+use graph::Graph;
+use graph::Node;
+use graph::Edge;
+
+mod parser;
+use parser::XmlParser;
+use parser::Filter;
 
 fn main() {
     println!("Hello, world!");
 
-    let a = graph::Node { id: 0, lat: 1.2345, lon: 5.4321 };
-    let b = graph::Node { id: 1, lat: 6.7890, lon: 0.1234 };
-    let edges = vec![graph::Edge { id: 42, src: a.id, dst: b.id }];
+    let a = Node { id: 0, lat: 1.2345, lon: 5.4321 };
+    let b = Node { id: 1, lat: 6.7890, lon: 0.1234 };
+    let edges = vec![Edge { id: 42, src: a.id, dst: b.id }];
     let nodes = vec![a, b];
-    let graph = graph::Graph { nodes: nodes, edges: edges };
+    let graph = Graph { nodes: nodes, edges: edges };
 
     println!("{}", graph);
+
+    let p = XmlParser { ..Default::default() };
+    p.apply().apply().apply();
+    println!();
 }
---
+++ src/parser/mod.rs
@@ -0,0 +1,77 @@
+use quick_xml;
+use quick_xml::Reader;
+use quick_xml::events::Event;
+
+pub struct XmlParser {
+    pub xml: String,
+}
+
+impl Default for XmlParser {
+    fn default() -> XmlParser {
+        return XmlParser {
+            xml: String::from(r#"
+                <tag1 att1 = "test">
+                    <tag2><!--Test comment-->TEST</tag2>
+                    <tag2>
+                        Test 2
+                    </tag2>
+                </tag1>
+            "#)
+        };
+    }
+}
+
+pub trait Filter {
+    fn apply(self) -> Self;
+}
+
+impl Filter for XmlParser {
+    fn apply(self) -> Self {
+        println!("{}", self.xml);
+        return self;
+    }
+}
+
+fn stuff() {
+    let xml = "";
+    let mut reader = Reader::from_str(xml);
+    reader.trim_text(true);
+
+    let mut count = 0;
+    let mut txt = Vec::new();
+    let mut buf = Vec::new();
+
+    // The `Reader` does not implement `Iterator` because it outputs borrowed data (`Cow`s)
+    loop {
+        match reader.read_event(&mut buf) {
+            Ok(Event::Start(ref e)) => {
+                println!("Event::Start(ref e) {:?}", String::from_utf8_lossy(e.name()));
+                match e.name() {
+                    b"tag1" => println!(
+                        "attributes values: {:?}",
+                        e.attributes().map(|a| a.unwrap().value).collect::<Vec<_>>()
+                    ),
+                    b"tag2" => count += 1,
+                    _ => (),
+                }
+            },
+            Ok(Event::Text(e)) => {
+                println!("Event::Text(e)");
+                txt.push(e.unescape_and_decode(&reader).unwrap())
+            },
+            Ok(Event::Eof) => {
+                println!("Event::Eof");
+                break
+            }, // exits the loop when reaching end of file
+            Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
+            _ => {
+                println!("nix");
+            }, // There are several other `Event`s we do not consider here
+        }
+
+        println!("{:?}", txt);
+
+        // if we don't keep a borrow elsewhere, we can clear the buffer to keep memory usage low
+        // buf.clear(); // probably wasted since "clear() has no effect on allocated capacity"
+    }
+}
