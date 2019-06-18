a26dd724d1d0aec97e8273f2a7617171406fb6dc
jena satkunarajan <st116472@stud.uni-stuttgart.de>
2019-06-18 20:36:30 +0200

    adds read function and offset calculation to graph

--- src/graph.rs
+++ src/graph.rs
@@ -1,4 +1,8 @@
 use std::fmt;
+use std::io;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::time::{Duration, Instant};
 
 pub struct Graph {
     pub nodes: Vec<Node>,
@@ -30,7 +34,7 @@ pub struct Node {
 
 impl fmt::Display for Node {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        return write!(f, "{{ id: {}, coord=({:.2}, {:.2}) }}", self.id, self.lat, self.lon);
+        return write!(f, "{{ id: {}, coord=({:.2}, {:.2}), edge_start: {}, edge_end: {} }}", self.id, self.lat, self.lon, self.edge_start, self.edge_end);
     }
 }
 
@@ -44,5 +48,99 @@ pub struct Edge {
 impl fmt::Display for Edge {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         return write!(f, "{{id: {}, source: {}, target: {}, length: {}}}", self.id, self. src, self.dest, self.weight);
+    }
+}
+
+pub trait Read {
+    fn read_graph(&mut self, file_name: &str) -> Result<(), io::Error>;
+}
+
+impl Read for Graph {
+    fn read_graph(&mut self, file_name: &str) -> Result<(), io::Error> {
+        let now = Instant::now();
+        let file = File::open(file_name)?;
+        let mut n_nodes;
+        let mut n_edges;
+        let mut i = 0;
+        let reader = BufReader::new(file);
+
+        let mut hax = 0;
+        for line in reader.lines() {
+            let curr_line = line.unwrap();
+            if curr_line == "" || curr_line.chars().next().unwrap() == '#' {
+                continue;
+            }
+            match i {
+                0 => {
+                    n_nodes = curr_line.parse::<usize>().unwrap();
+                    self.nodes.reserve(n_nodes);
+                    self.node_count = n_nodes;
+                },
+                1 => {
+                    n_edges = curr_line.parse::<usize>().unwrap();
+                    self.edges.reserve(n_edges);
+                    self.edge_count = n_edges;
+                },
+                j if j > 1 && j <= self.node_count + 1 => {
+                    let line_string = curr_line.split_whitespace();
+                    let param: Vec<&str> = line_string.collect(); 
+                    self.nodes.push(Node {
+                        id: param[0].parse::<usize>().unwrap(),
+                        lat: param[2].parse::<f64>().unwrap(),
+                        lon: param[3].parse::<f64>().unwrap(),
+                        edge_start: 0,
+                        edge_end: 0
+                    });
+                },
+                j if j > self.node_count + 1 => {
+                    let line_string = curr_line.split_whitespace();
+                    let param: Vec<&str> = line_string.collect(); 
+                    self.edges.push(Edge {
+                        id: hax,
+                        src: param[0].parse::<usize>().unwrap(),
+                        dest: param[1].parse::<usize>().unwrap(),
+                        weight: param[2].parse::<usize>().unwrap()
+                    });
+                    hax += 1;
+                }
+
+                _ => {}
+            }
+            i += 1;
+        }
+        println!("Read graph in {} microseconds a.k.a. {} seconds", now.elapsed().as_micros(), now.elapsed().as_secs());
+        Ok(())
+    }
+}
+
+pub trait EdgeOffset {
+    fn set_edge_offset(&mut self);
+}
+
+impl EdgeOffset for Graph {
+    fn set_edge_offset(&mut self){
+        let now = Instant::now();
+        let l = self.node_count;
+        let mut i = 0;
+        let mut j = 0;
+        for edge in self.edges.iter() {
+            if edge.src == self.nodes[j].id {
+                self.nodes[j].edge_end = i;
+            }
+            else {
+                j += 1;
+                while j < l && edge.src != self.nodes[j].id {
+                    self.nodes[j].edge_start = i - 1;
+                    self.nodes[j].edge_end = i - 1;
+                    j += 1;
+                }
+                if j < l {
+                    self.nodes[j].edge_start = i;
+                    self.nodes[j].edge_end = i;
+                }
+            }
+            i += 1;
+        }
+        println!("Set offset in {} microseconds a.k.a. {} seconds", now.elapsed().as_micros(), now.elapsed().as_secs());
     }
 }
