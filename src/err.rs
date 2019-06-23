934af955360dcc18b496d2d912aec75db28e3b2b
Parga Cacheiro, Dominic <dominic.parga@gmail.com>
2019-06-23 20:45:45 +0200

    Refactor err-module

--- src/err.rs
+++ src/err.rs
@@ -8,17 +8,33 @@ use quick_xml;
 #[derive(Debug)]
 pub enum Error {
     Custom(String),
-    UnsuppExt(String),
-    Io(io::Error),
-    InvalidUnicode(String),
-    XmlIo(quick_xml::Error),
 }
 
 impl Error {
     pub fn custom(msg: &str) -> Self {
         Error::Custom(String::from(msg))
     }
+}
 
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Error::Custom(msg) => msg.fmt(f),
+        }
+    }
+}
+
+//--------------------------------------------------------------------------------------------------
+
+#[derive(Debug)]
+pub enum FileError {
+    UnsuppExt(String),
+    Io(io::Error),
+    InvalidUnicode(String),
+    XmlIo(quick_xml::Error),
+}
+
+impl FileError {
     pub fn unsupported_extension(ext: &str, supported: &[&str]) -> Self {
         let mut msg = {
             if ext.is_empty() {
@@ -31,36 +47,33 @@ impl Error {
         msg = format!("{} Please use a valid path to the osm-file.", msg);
         msg = format!("{}\nSupported extensions are: {:?}", msg, supported);
 
-        Error::UnsuppExt(msg)
+        FileError::UnsuppExt(msg)
     }
 
     pub fn invalid_unicode() -> Self {
-        Error::InvalidUnicode(String::from("File name is invalid Unicode."))
+        FileError::InvalidUnicode(String::from("File name is invalid Unicode."))
     }
 }
 
-impl fmt::Display for Error {
+impl fmt::Display for FileError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            Error::Custom(msg) => msg.fmt(f),
-            Error::UnsuppExt(msg) => msg.fmt(f),
-            Error::Io(e) => e.fmt(f),
-            Error::XmlIo(e) => e.fmt(f),
-            Error::InvalidUnicode(msg) => msg.fmt(f),
+            FileError::UnsuppExt(msg) => msg.fmt(f),
+            FileError::Io(e) => e.fmt(f),
+            FileError::XmlIo(e) => e.fmt(f),
+            FileError::InvalidUnicode(msg) => msg.fmt(f),
         }
     }
 }
 
-//--------------------------------------------------------------------------------------------------
-
-impl From<io::Error> for Error {
+impl From<io::Error> for FileError {
     fn from(e: io::Error) -> Self {
-        Error::Io(e)
+        FileError::Io(e)
     }
 }
 
-impl From<quick_xml::Error> for Error {
+impl From<quick_xml::Error> for FileError {
     fn from(e: quick_xml::Error) -> Self {
-        Error::XmlIo(e)
+        FileError::XmlIo(e)
     }
 }
