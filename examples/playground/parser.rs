d849a90907c4e5f99aef4a914d0bc099220016ba
Parga Cacheiro, Dominic <dominic.parga@gmail.com>
2019-06-25 00:49:35 +0200

    Add time-measurement to parsing-example

--- examples/playground/parser.rs
+++ examples/playground/parser.rs
@@ -1,4 +1,5 @@
 use std::ffi::OsString;
+use std::time::Instant;
 
 use osmgraphing::osm;
 
@@ -8,20 +9,25 @@ fn main() {
         None => OsString::from("resources/osm/small.fmi"),
     };
 
+    println!("Start parsing...");
+    let now = Instant::now();
     let graph = match osm::Support::from_path(&path) {
         Ok(osm::Support::PBF) => {
             let parser = osm::pbf::Parser;
             parser.parse(&path)
-        },
+        }
         Ok(osm::Support::FMI) => {
             let parser = osm::fmi::Parser;
             parser.parse(&path)
-        },
-        Ok(osm::Support::XML) => {
-            unimplemented!()
-        },
+        }
+        Ok(osm::Support::XML) => unimplemented!(),
         Err(e) => panic!("{:}", e),
     };
-
+    println!(
+        "Finished in {} seconds ({} ms).",
+        now.elapsed().as_secs(),
+        now.elapsed().as_micros(),
+    );
+    println!();
     println!("{}", graph);
 }
