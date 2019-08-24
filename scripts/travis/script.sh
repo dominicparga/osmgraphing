2064a737ea6b62c43e60827cf6445993797ca90b
Parga Cacheiro, Dominic <dominic.parga@gmail.com>
2019-08-24 20:36:43 +0200

    Remove colors and debug-echos

--- scripts/travis/script.sh
+++ scripts/travis/script.sh
@@ -10,10 +10,8 @@ source ./scripts/travis/helper.sh
 # -> check versions before everything to save runtime
 if [[ -n "${TRAVIS_TAG}" ]]; then
     OSMGRAPHING_VERSION="v$(cat ./Cargo.toml | grep 'version' | sed 's_.*version.*"\(.*\)".*_\1_')"
-    echo "OSMGRAPHING_VERSION=${OSMGRAPHING_VERSION}"
-    echo "TRAVIS_TAG=${TRAVIS_TAG}"
     if [[ "${TRAVIS_TAG}" != "${OSMGRAPHING_VERSION}" ]]; then
-        echo -e "${RED}The version in 'Cargo.toml' doesn't match the provided tag '${TRAVIS_TAG}'.${NC}"
+        echo "The version in 'Cargo.toml' doesn't match the provided tag '${TRAVIS_TAG}'."
         exit 1
     fi
 fi
