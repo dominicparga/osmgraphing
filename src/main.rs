14b728d6172eb5515778c91897f032235510e842
Parga Cacheiro, Dominic <dominic.parga@gmail.com>
2019-09-09 10:40:39 +0200

    Integrate basic cmdline-parser

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
+use clap;
+
+fn parse_cmdline<'a>() -> clap::ArgMatches<'a> {
+    clap::App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .long_about(
+            (&[
+                "",
+                "For all supported example files, please use",
+                "    `cargo run --example`",
+                "",
+                "You can set up the logger by setting RUST_LOG, e.g. to",
+                "    export RUST_LOG='warn,osmgraphing=info,parser=info,dijkstra=info'",
+                "",
+                "for getting `warn`s per default, but `info` about the others (e.g. `parser`).",
+            ]
+            .join("\n"))
+                .as_ref(),
+        )
+        .get_matches()
+}
+
 fn main() {
     env_logger::Builder::from_env("RUST_LOG").init();
 
-    println!("For all supported example files, please use");
-    println!("");
-    println!("    'cargo run --example'");
-    println!("");
-    println!("");
-    println!("You can set up the logger by setting RUST_LOG, e.g. to");
-    println!("");
-    println!("    export RUST_LOG='warn,osmgraphing=info,parser=info,dijkstra=info'");
-    println!("");
-    println!(
-        "for getting warnings per default, but info about osmgraphing and the 'parser'-example."
-    );
+    let _matches = parse_cmdline();
+    println!("{:?}", _matches);
 }
