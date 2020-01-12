e8809e65026421eb31ea076d94e552ab7173d5d0
Parga Cacheiro, Dominic <dominic.parga@gmail.com>
2020-01-12 14:45:23 +0100

    Add scripts to execute braess-optimization

---
+++ scripts/braess/gen_proto_routes.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+#------------------------------------------------------------------------------#
+# prepare directories for simulation-inputs and -outputs
+
+__MAP='stuttgart-regbez_2019-09-07.osm.pbf'
+# truncate extensions
+__OUT_DIR="custom/out/braess/${__MAP%%.*}"
+
+#------------------------------------------------------------------------------#
+# clear console, cleanup project and build+run simulation
+
+clear
+
+cargo fmt
+
+mkdir -v -p "${__OUT_DIR}"
+
+cargo run --release -- \
+--verbose \
+gen-proto-routes \
+--seed 42 \
+--route-count 30000 \
+--map "custom/resources/maps/${__MAP}" \
+--out "${__OUT_DIR}/proto_routes_30k.csv" \
