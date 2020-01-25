1fcad59ec8ba785dd0bb2019f12fa23d88f83ea9
Parga Cacheiro, Dominic <dominic.parga@gmail.com>
2020-01-25 17:39:22 +0100

    Remove stuff related to braess-optimization

---
+++ src/bin/osmgraphing.rs
@@ -0,0 +1,74 @@
+//------------------------------------------------------------------------------------------------//
+// other modules
+
+//------------------------------------------------------------------------------------------------//
+// own modules
+
+//------------------------------------------------------------------------------------------------//
+
+fn parse_cmdline<'a>() -> clap::ArgMatches<'a> {
+    // arg: quiet
+    let tmp = &[
+        "Permits logging 'info', but only 'warn' and 'error'.",
+        "The env-variable 'RUST_LOG' has precedence.",
+    ]
+    .join("\n");
+    let arg_quiet = clap::Arg::with_name("quiet")
+        .short("q")
+        .long("quiet")
+        .help(tmp);
+
+    // all
+    clap::App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .long_about(
+            (&[
+                "",
+                "You can set up the logger by setting RUST_LOG, e.g. to",
+                "    export RUST_LOG='warn,osmgraphing=info,parser=info,astar=info'",
+                "for getting 'warn's per default, but 'info' about the others (e.g. 'parser').",
+                "RUST_LOG is set up automatically, setting RUST_LOG to 'info'",
+                "for relevant parts of the software, but consider the flag '--quiet'.",
+                "",
+                "In case you're using cargo, please use",
+                "    cargo run --example",
+                "for all supported example files",
+            ]
+            .join("\n"))
+                .as_ref(),
+        )
+        .arg(arg_quiet)
+        .get_matches()
+}
+
+fn setup_logging(quietly: bool) {
+    let mut builder = env_logger::Builder::new();
+    // minimum filter-level: `warn`
+    builder.filter(None, log::LevelFilter::Warn);
+    // if quiet logging: don't log `info` for the server and this repo
+    if !quietly {
+        builder.filter(Some(env!("CARGO_PKG_NAME")), log::LevelFilter::Info);
+    }
+    // overwrite default with environment-variables
+    if let Ok(filters) = std::env::var("RUST_LOG") {
+        builder.parse_filters(&filters);
+    }
+    if let Ok(write_style) = std::env::var("RUST_LOG_STYLE") {
+        builder.parse_write_style(&write_style);
+    }
+    // init
+    builder.init();
+}
+
+fn main() -> Result<(), ()> {
+    let matches = parse_cmdline();
+    setup_logging(matches.is_present("quiet"));
+
+    if matches.args.len() == 0 {
+        println!("Execute '.../osmgraphing --help' (or 'cargo run -- --help') for more info.");
+    }
+
+    Ok(())
+}
