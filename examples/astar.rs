9a6a772dc59b52008db1207e8f22e6be33ef32a2
Parga Cacheiro, Dominic <dominic.parga@gmail.com>
2020-02-10 02:14:05 +0100

    Generate random routes in astar-example

--- examples/astar.rs
+++ examples/astar.rs
@@ -1,5 +1,7 @@
 use log::{error, info};
 use osmgraphing::{network::NodeIdx, routing, Parser};
+use rand::distributions::{Distribution, Uniform};
+use rand::SeedableRng;
 use std::ffi::OsString;
 use std::time::Instant;
 
@@ -77,14 +79,21 @@ fn main() {
     let nodes = graph.nodes();
     let mut astar = routing::factory::astar::unidirectional::shortest();
 
-    let src_idx = NodeIdx::zero();
-    let dsts: Vec<NodeIdx> = (0..nodes.count()).map(NodeIdx::new).collect();
-    // let dsts: Vec<NodeIdx> = (185..199).map(nNodeIdx::new).collect();
-    // let dsts: Vec<usize> = vec![80]; problem on baden-wuerttemberg.osm.pbf
+    // generate random route-pairs
+    let route_count = 100;
+    let seed = 42;
+    let mut rng = rand_pcg::Pcg32::seed_from_u64(seed);
+    let die = Uniform::from(0..nodes.count());
+    let mut routes = vec![];
+    for _ in 0..route_count {
+        let src_idx = NodeIdx::new(die.sample(&mut rng));
+        let dst_idx = NodeIdx::new(die.sample(&mut rng));
+        routes.push((src_idx, dst_idx));
+    }
 
-    let src = nodes.create(src_idx);
-
-    for dst_idx in dsts {
+    // calculate best paths
+    for (src_idx, dst_idx) in routes {
+        let src = nodes.create(src_idx);
         let dst = nodes.create(dst_idx);
 
         info!("");
