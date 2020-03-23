3ff7728f994090bdbb983e70be28a39a82cdf3ef
Parga Cacheiro, Dominic <dominic.parga@gmail.com>
2020-03-23 17:42:03 +0100

    Improve default-accuracy

--- src/defaults.rs
+++ src/defaults.rs
@@ -1,15 +1,17 @@
 pub mod accuracy {
     /// value is good because it refers to
-    /// - 1 cm for km
-    /// - 10 µs for s
+    /// - 1 mm for km
+    /// - 1 µs for s
+    /// - 60 µs for min
+    /// - 3.6 ms for hour
     /// - lat/lon
     ///   - lat: 1/60 ~ 0.0167 degrees equals 1_852 m
-    ///     -> 1e-5 degrees equals around 1.1 m
+    ///     -> 1e-6 degrees equals around 0.11 m
     ///   - lon: distance depends on latitude
-    ///     -> 1e-5 degrees equals <= 1.1 m (equator)
+    ///     -> 1e-6 degrees equals <= 0.11 m (equator)
     ///   -> 1e-5 degrees points to a person in a room, see https://xkcd.com/2170/
-    pub const F64_ABS: f64 = 0.000_01; // = 10^(-F64__FMT_DIGITS)
-    pub const F64_FMT_DIGITS: usize = 5;
+    pub const F64_ABS: f64 = 0.000_001; // = 10^(-F64__FMT_DIGITS)
+    pub const F64_FMT_DIGITS: usize = 6;
 }
 
 pub mod speed {
