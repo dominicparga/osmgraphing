60ef5e692a2863ba08c5c658ff8a61f9c0e13d43
Parga Cacheiro, Dominic <dominic.parga@gmail.com>
2020-05-18 14:58:47 +0200

    Add small error-module

---
+++ src/helpers/err.rs
@@ -0,0 +1,33 @@
+use std::{
+    fmt::{self, Display},
+    io,
+};
+
+pub type Feedback = Result<(), Msg>;
+
+#[derive(Debug)]
+pub struct Msg(String);
+
+impl Display for Msg {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl From<io::Error> for Msg {
+    fn from(e: io::Error) -> Msg {
+        Msg(format!("{}", e))
+    }
+}
+
+impl From<String> for Msg {
+    fn from(s: String) -> Msg {
+        Msg(s)
+    }
+}
+
+impl From<&str> for Msg {
+    fn from(s: &str) -> Msg {
+        Msg(s.to_owned())
+    }
+}
