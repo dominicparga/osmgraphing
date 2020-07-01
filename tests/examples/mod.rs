a18b62a3464064bd4781411778e443a58296061a
Parga Cacheiro, Dominic <dominic.parga@gmail.com>
2020-07-01 12:53:18 +0200

    Refactor tests for examples a little

--- tests/examples/mod.rs
+++ tests/examples/mod.rs
@@ -1,33 +1,31 @@
-#[allow(dead_code)]
-mod dijkstra {
-    include!("../../examples/dijkstra.rs");
-
-    pub fn test() {
-        match run() {
-            Ok(()) => (),
-            Err(msg) => panic!("{}", msg),
-        }
-    }
-}
-
 #[test]
 fn dijkstra() {
-    dijkstra::test();
-}
+    #[allow(dead_code)]
+    mod example {
+        include!("../../examples/dijkstra.rs");
 
-#[allow(dead_code)]
-mod parser {
-    include!("../../examples/parser.rs");
-
-    pub fn test() {
-        match run() {
-            Ok(()) => (),
-            Err(msg) => panic!("{}", msg),
+        pub fn test() {
+            match run() {
+                Ok(()) => (),
+                Err(msg) => panic!("{}", msg),
+            }
         }
     }
+    example::test();
 }
 
 #[test]
 fn parser() {
-    parser::test();
+    #[allow(dead_code)]
+    mod example {
+        include!("../../examples/parser.rs");
+
+        pub fn test() {
+            match run() {
+                Ok(()) => (),
+                Err(msg) => panic!("{}", msg),
+            }
+        }
+    }
+    example::test();
 }
